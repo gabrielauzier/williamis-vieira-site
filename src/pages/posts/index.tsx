3af060e85f3bb23e2eb1fd5fe146d10f63ccea0f
gabvask <gabrielvascoauzier@gmail.com>
2022-06-05 22:35:06 -0400

    feat: add posts page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Learna & Yark Workspace</strong>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.{" "}
+            </p>
+          </a>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Learna & Yark Workspace</strong>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.{" "}
+            </p>
+          </a>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Learna & Yark Workspace</strong>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.{" "}
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
