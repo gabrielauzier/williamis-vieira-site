449b9004c1f341206a2ecc42ff3b2b36d07c5463
gabvask <gabrielvascoauzier@gmail.com>
2022-06-09 16:35:11 -0400

    refactor: small changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { Header } from "../components/Header";
 import { SessionProvider } from "next-auth/react";
 
 import "../styles/global.scss";
+import { Footer } from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </SessionProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,23 @@
 import Head from "next/head";
-import { SubscribeButton } from "../components/SubscribeButton";
+import { AboutSection } from "../components/AboutSection";
+import { ContactSection } from "../components/ContactSection";
 
+import { HeroSection } from "../components/HeroSection";
 import styles from "./home.module.scss";
 
-import { GetStaticProps } from "next";
-import { stripe } from "../services/stripe";
-
-interface HomeProps {
-  product: {
-    priceId: string;
-    amount: number;
-  };
-}
-
-function Home({ product }: HomeProps) {
+function Home() {
   return (
     <>
       <Head>
         <title>Home | Ignews</title>
       </Head>
-      <main className={styles.contentContainer}>
-        <section className={styles.hero}>
-          <span>👏 Hey, welcome</span>
-          <h1>
-            News about the <span>React</span> world.
-          </h1>
-          <p>
-            Get access to all the publications <br />
-            <span>for {product.amount} /month</span>
-          </p>
-          <SubscribeButton />
-        </section>
-
-        <img src="/images/avatar.svg" alt="Girl coding" />
-      </main>
+      <div className={styles.container}>
+        <HeroSection />
+        <AboutSection />
+        <ContactSection />
+      </div>
     </>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve(
-    process.env.STRIPE_PRODUCT_PRICE_ID!
-  );
-
-  const product = {
-    priceId: price.id,
-    amount: new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    }).format(price.unit_amount! / 100),
-  };
-
-  return {
-    props: {
-      product,
-    },
-  };
-};
 
 export default Home;
